6dae98cfbb5104926e66dcd73f6874f11672b2bf
EvaSeidlmayer <seidlamyer@zbmed.de>
2025-04-10 23:47:52 +0200

    add print statement for debugging

--- app.py
+++ app.py
@@ -80,6 +80,7 @@ def get_locale():
     return session.get("language") or request.accept_languages.best_match(["en", "de"])
 
 babel.init_app(app, locale_selector=get_locale)
+logger.info(f"DEBUG mode is {'on' if app.debug else 'off'}")
 
 @app.route("/")
 async def home():
